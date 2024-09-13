071a6b81689470685adbdf14f640ef8c75bba8a6
Subin Hong <subin5923@gmail.com>
2024-09-13 15:32:38 +0900

    주석추가

--- baekjoon_ex1.py
+++ baekjoon_ex1.py
@@ -1,7 +1,7 @@
-S = input()
-b=[0 for i in range(26)]
-for num in S:
-    b[ord(num)-ord('a')] +=1
-print(" ".join(map(str, b))) 
+arr=input()
+cnt=[0]*26
 
-#map 함수 - 자료형 형변환
+for i in arr:
+    cnt[ord(i)-97]+=1
+
+print(*cnt)
--- baekjoon_ex2.py
+++ baekjoon_ex2.py
@@ -1,12 +1,9 @@
-#num_list=[input(_) for _ in range(3)]
-A=int(input())
-B=int(input())
-C=int(input())
-#mul=1
-mul=A*B*C
+num_list=[input() for _ in range(3)]
+# 변수 미사용 시, _는 unpack 값으로 사용 가능
+mul=1
 count=[0 for i in range(10)]
-#for i in num_list:
-    #mul*=int(i)
+for i in num_list:
+    mul*=int(i)
 for j in str(mul):
     count[ord(j)-ord('0')]+=1
 print('\n'.join(map(str,count)))
